975e5f99e203b6bda08669fb1b6f037aeb2f5e6d
Etumi <etumi12@gmail.com>
2020-05-18 14:56:08 -0400

    version 4 - make h barplot responsive to dropdown, clean app.js a bit

--- static/js/app.js
+++ static/js/app.js
@@ -1,78 +1,85 @@
 //Populate drop down menu with Sample ID numbers
-d3.json("samples.json").then(function(data){
-    var samples = data.samples;
+function buildDropdown() {
+    d3.json("samples.json").then(function(data){
+        var samples = data.samples;
 
-    var selector = d3.select("#subjectID")
-    //console.log(selector);
+        var selector = d3.select("#subjectID")
+        //console.log(selector);
 
-    samples.forEach(sample => {
-        selector.append("option").text(sample.id);
-    });     
-});
+        samples.forEach(sample => {
+            selector.append("option").text(sample.id);
+        });     
+    });
+};
 
-
+buildDropdown();
 
 // Read in belly buttn biodiversity data 
 function buildBarPlot() {
-    d3.json("samples.json").then(function(data){
-        console.log("master dataset");
-        console.log(data);
-        var data_samples = data.samples;
-        console.log("samples dict from big data source")
-        console.log(data_samples);
+    //var inputValue = d3.select("#subjectID").property("value");
+    //console.log(`dropdown value ${inputValue}`);
+
+    d3.json("samples.json").then(function(data, inputValue){
+        var samples = data.samples;
+
+        var inputValue = d3.select("#subjectID").property("value");
+        console.log(`dropdown value ${inputValue}`);
+
+        // console.log("master dataset");
+        // console.log(data);
+
+        var selectedSampleData = samples.filter(obj => obj.id === inputValue);
+
+        console.log(selectedSampleData);
+        
+        //console.log("samples dict from big data source")
+        //console.log(samples);
 
         // Sort dataset
-        var data_samples_copy = data_samples;
-        //console.log(data_samples_copy);
-        var test = [data_samples[0]];
-        //console.log('test');
-        //console.log(test);
-        //console.log(data.samples[0])
-        var sortedSamples = test.sort((first, second) =>  second.sample_values - first.sample_values);
-        sortedSamples = sortedSamples[0];
-        //console.log('sortedSamples');
-        //console.log(sortedSamples);
-
+        var sortedSample = selectedSampleData.sort((first, second) =>  second.sample_values - first.sample_values);
+        sortedSample = sortedSample[0];
+        //console.log('sortedSample');
+        //console.log(sortedSample);
         
-        sortedSamples['sample_values'] = sortedSamples['sample_values'].slice(0,10).reverse();
-        sortedSamples['otu_ids'] = sortedSamples['otu_ids'].slice(0, 10).reverse();
-        sortedSamples['otu_labels'] = sortedSamples['otu_labels'].slice(0, 10).reverse();
-        //console.log('sliced sortedSamples');
-        //console.log(sortedSamples);
+        sortedSample['sample_values'] = sortedSample['sample_values'].slice(0,10).reverse();
+        sortedSample['otu_ids'] = sortedSample['otu_ids'].slice(0, 10).reverse();
+        sortedSample['otu_labels'] = sortedSample['otu_labels'].slice(0, 10).reverse();
+        //console.log('sliced sortedSample');
+        //console.log(sortedSample);
 
         //console.log("sample values");
-        //console.log(sortedSamples.sample_values);
+        //console.log(sortedSample.sample_values);
 
-        sortedSamples['otu_ids_2'] = sortedSamples['otu_ids'].map(id => `OTU ${id}`)
+        sortedSample['otu_ids_2'] = sortedSample['otu_ids'].map(id => `OTU ${id}`)
         //console.log("otu_ids");
-        //console.log(sortedSamples.otu_ids_2);
+        //console.log(sortedSample.otu_ids_2);
 
         var trace = [{
             type: "bar",
             orientation: "h",
-            x: sortedSamples.sample_values,
-            y: sortedSamples.otu_ids_2,
-            text: sortedSamples.otu_labels,
+            x: sortedSample.sample_values,
+            y: sortedSample.otu_ids_2,
+            text: sortedSample.otu_labels,
         }];
 
         var layout1 = {
-            title: "Top 10 OTU's"
+            title: `Sample ${inputValue}'s Top 10 OTU's`
         };
 
         Plotly.newPlot("h-plot", trace, layout1);
-
     });
 };
 
 buildBarPlot();
 
+
 function buildBubblePlot() {
     d3.json("samples.json").then(function(data){
 
-        var data_samples = data.samples.slice(0,3);
-        //console.log(data_samples);
+        var samples = data.samples.slice(0,3);
+        //console.log(samples);
 
-        var data = data_samples.map(sample => {
+        var data = samples.map(sample => {
             
             var trace = {
                 mode: "markers",
@@ -91,21 +98,6 @@ function buildBubblePlot() {
             return trace;
         });
 
-        //console.log("data for bubble graphs");
-        //console.log(data);
-
-
-        // var trace2 = [{
-        //     mode: "markers",
-        //     y: sortedSamples.sample_values,
-        //     x: sortedSamples.otu_ids,
-        //     text: sortedSamples.otu_labels,
-        //     marker: {
-        //         color: sortedSamples.otu_ids,
-        //         size: sortedSamples.sample_values
-        //     }
-        // }];
-
         var layout2 = {
             xaxis: {title: "OTU ID"},
             showlegend: false
@@ -116,4 +108,6 @@ function buildBubblePlot() {
     });
 };
 
-buildBubblePlot()
+buildBubblePlot();
+
+d3.select("#subjectID").on("change", buildBarPlot);
