c76fd6ca5fe11f53c0060abacdd125584add94be
Etumi <etumi12@gmail.com>
2020-05-19 16:21:09 -0400

    version 6.2 - make bubble plot responsive to sample selection

--- static/js/app.js
+++ static/js/app.js
@@ -45,10 +45,12 @@ function buildBarPlot() {
             x: sortedSample.sample_values,
             y: sortedSample.otu_ids_2,
             text: sortedSample.otu_labels,
+            textfont: {family: "Cambria"}
         }];
 
         var layout1 = {
-            title: `Sample ${inputValue}'s Top 10 OTU's`
+            title: `Sample ${inputValue}'s Top 10 OTU's`,
+            font: {family: "Cambria"}
         };
 
         Plotly.newPlot("h-plot", trace, layout1);
@@ -58,14 +60,20 @@ function buildBarPlot() {
 buildBarPlot();
 
 //d3.select("#subjectID").on("change", buildBarPlot);
-
+// Build the bubble plot
 function buildBubblePlot() {
     d3.json("samples.json").then(function(data){
 
-        var samples = data.samples.slice(0,3);
-        //console.log(samples);
+        var samples = data.samples;
 
-        var data = samples.map(sample => {
+        var inputValue = d3.select("#subjectID").property("value");
+        // console.log(inputValue);
+
+        // Get object of the selected id
+        var selectedSampleData = samples.filter(obj => obj.id === inputValue);
+        console.log(selectedSampleData);
+
+        var data = selectedSampleData.map(sample => {
             
             var trace = {
                 mode: "markers",
@@ -78,23 +86,83 @@ function buildBubblePlot() {
                     sizeref: 2
                     //sizeref: 2.0 * Math.max(sample.sample_values) / (2**2),
                     //sizemode: 'area'
-                }
+                },
+                textfont: {family: "Cambria"}
             };
 
             return trace;
         });
 
+        console.log(data);
+
         var layout2 = {
             xaxis: {title: "OTU ID"},
-            showlegend: false
+            showlegend: false,
+            title : `All OTUs for Sample ${inputValue}`,
+            font: {family: "Cambria"}
         };
 
-        Plotly.newPlot("marker-plot", data, layout2);
+        Plotly.newPlot("bubble-plot", data, layout2);
 
     });
 };
 
 buildBubblePlot();
+
+// build the gauge plot
+function buildGuagePlot(){
+    var data = [
+        {
+            domain: { x: [0, 1], y: [0, 1] },
+            //value: 3,
+            title: { text: "Belly Button Washing Frequency",
+                    align: "center",
+                    font: {family: "Cambria"}
+            },
+            type: "indicator",
+            mode: "gauge",
+            text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9"],
+            gauge: {
+                axis: { range: [0, 9]
+                        // tickmode: "auto",
+                        // tickvalue: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                        // ticktext: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9"],
+                        // ticks: "inside",
+                        // ticklen: "2px",
+                        // tickwidth: "2px",
+                        // tickcolor: "#444",
+                        // tickfont: {family: "Cambria"}
+                        //nticks: 9
+                },
+                bordercolor: "f8f8ff",
+                steps: [
+                { range: [0, 1], color: "red", name: "0-1", "0-1": {enabled: true}},
+                { range: [1, 2], color: "gray", name: "1-2"},
+                { range: [2, 3], color: "blue", name: "2-3"},
+                { range: [3, 4], color: "pink", name: "3-4"},
+                { range: [4, 5], color: "yellow", name: "4-5"},
+                { range: [5, 6], color: "purple", name: "5-6"},
+                { range: [6, 7], color: "coral", name: "6-7"},
+                { range: [7, 8], color: "lightgray", name: "7-8"},
+                { range: [8, 9], color: "violet", name: "8-9"}
+                ],
+                threshold: {
+                line: { color: "red", width: 4 },
+                thickness: 0.75,
+                value: 3
+                }
+            }
+        }
+    ];
+
+    var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+    
+    Plotly.newPlot('gauge-plot', data, layout);
+};
+
+// buildGuagePlot();
+
+
 //-------------------------------------------------------------------------//
 
 //--------------------------Demographics info-----------------------------//
@@ -137,6 +205,7 @@ buildDemoInfo();
 function updateDashInfo(){
     buildDemoInfo();
     buildBarPlot();
+    buildBubblePlot();
 };
 
 d3.select("#subjectID").on("change", updateDashInfo);
